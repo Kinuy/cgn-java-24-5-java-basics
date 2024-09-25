69321588abd610e6f28533feb93bc1c41859b723
Tobias Weber <toby-weber@gmx.de>
2024-09-25 17:03:46 +0200

    formatting

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -13,33 +13,37 @@ public class Calculator {
     public static int sum(int a, int b) {
         return a + b;
     }
+
     public static boolean isEven(int a) {
-        if (a % 2 == 0){
+        if (a % 2 == 0) {
             return true;
         }
+
 
         return false;
     }
 
-    public static int product(int a,int b) {
+    public static int product(int a, int b) {
         return a * b;
     }
+
     public static String toUpperCase(String a) {
         return a.toUpperCase();
     }
+
     public static boolean isPositive(int a) {
-        return a>0;
+        return a > 0;
     }
 
     public static String checkText(String message) {
-        String[] greetingsToCheck = {"hello","ciao","salut ","hallo","hola","ahoj","czesc"};
+        String[] greetingsToCheck = {"hello", "ciao", "salut ", "hallo", "hola", "ahoj", "czesc"};
         String answer = "";
-        for(int i=0;i<greetingsToCheck.length;i++){
-            if(message.contains(greetingsToCheck[i])) {
+        for (int i = 0; i < greetingsToCheck.length; i++) {
+            if (message.contains(greetingsToCheck[i])) {
                 return "greetings found: " + greetingsToCheck[i];
             }
         }
-    return "nothing found in message";
+        return "nothing found in message";
 
     }
 }
