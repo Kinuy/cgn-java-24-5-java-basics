ee360082787a7376d212367d5df4e703e67505b2
Tobias Weber <toby-weber@gmx.de>
2024-09-25 12:09:42 +0200

    Tag 3 bonus task

--- src/main/java/org/example/Prog.java
+++ src/main/java/org/example/Prog.java
@@ -1,21 +1,44 @@
+
 package org.example;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Prog {
 
 
     public static void main(String[] args) {
 
-//        System.out.println(add(1, 2));
-//        System.out.println(substract(1, 2));
-//        double a = 1, b = 2;
-//        if (a > b) {
-//            System.out.println(substract(a, b));
-//        } else {
-//            System.out.println(add(a, b));
-//        }
-//        isEven(a);
+/*        System.out.println(add(1, 2));
+        System.out.println(substract(1, 2));
+        double a = 1, b = 2;
+        if (a > b) {
+            System.out.println(substract(a, b));
+        } else {
+            System.out.println(add(a, b));
+        }
+        isEven(a);
         String input = "Ein eis esse sie nie";
-        System.out.println(isPalindrom(input));
+        System.out.println(isPalindrom(input));*/
+        int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+        int sum = 0;
+        for(int number: numbers){
+            System.out.println(number);
+            sum += number;
+        }
+        System.out.println(sum);
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Provide the string: ");
+        String argument = scanner.nextLine();
+        System.out.print("Input is: " + argument + "\n");
+        for(int i = 0; i < argument.length(); i++){
+            System.out.println(argument.charAt(i));
+        }
+
+
+
     }
 
     public static double add(double a, double b) {
