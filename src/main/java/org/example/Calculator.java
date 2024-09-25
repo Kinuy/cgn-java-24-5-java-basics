f4b735ac82c7854912b60fe1f2f8ee855bf4eb43
Tobias Weber <toby-weber@gmx.de>
2024-09-25 16:37:49 +0200

    implement function checkText

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,7 +2,11 @@ package org.example;
 
 public class Calculator {
     public static void main(String[] args) {
-        int result = sum(3,6);
+//        int result = sum(3,6);
+
+        String message = "Hi my ciao name is hellu.";
+        String checkMessage = checkText(message);
+        System.out.println(checkMessage);
     }
 
 
@@ -25,5 +29,17 @@ public class Calculator {
     }
     public static boolean isPositive(int a) {
         return a>0;
+    }
+
+    public static String checkText(String message) {
+        String[] greetingsToCheck = {"hello","ciao","salut ","hallo","hola","ahoj","czesc"};
+        String answer = "";
+        for(int i=0;i<greetingsToCheck.length;i++){
+            if(message.contains(greetingsToCheck[i])) {
+                return "greetings found: " + greetingsToCheck[i];
+            }
+        }
+    return "nothing found in message";
+
     }
 }
