54b69dfa8e9cda578c855fd46e8841b5b5b850a3
Tobias Weber <toby-weber@gmx.de>
2024-09-25 15:38:42 +0200

    implement functions and according tests

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -9,4 +9,21 @@ public class Calculator {
     public static int sum(int a, int b) {
         return a + b;
     }
+    public static boolean isEven(int a) {
+        if (a % 2 == 0){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static int product(int a,int b) {
+        return a * b;
+    }
+    public static String toUpperCase(String a) {
+        return a.toUpperCase();
+    }
+    public static boolean isPositive(int a) {
+        return a>0;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,11 +1,14 @@
 import org.example.Calculator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class CalculatorTest {
 
     @Test
+    @DisplayName("test")
     public void sumTest_whenValue3and6_expectValue9() {
         //GIVEN
         int a = 3;
@@ -17,5 +20,60 @@ public class CalculatorTest {
 
         //THEN
         assertEquals(9,actual);
+    }
+
+    @Test
+    @DisplayName("isEvenTest")
+    public void isEven_whenValue3_expectValueFalse() {
+        //GIVEN
+        int a = 3;
+
+        //WHEN
+        boolean actual = Calculator.isEven(a);
+
+        //THEN
+        assertFalse(actual);
+//        assertEquals(false,actual);
+    }
+
+    @Test
+    @DisplayName("productTest")
+    public void productTest_whenValue2And2_expectValue4() {
+        //GIVEN
+        int a = 2;
+        int b = 2;
+
+        //WHEN
+        int actual = Calculator.product(a,b);
+
+        //THEN
+        assertEquals(4,actual);
+
+    }
+    @Test
+    @DisplayName("toUpperCaseTest")
+    public void toUpperCaseTest_whenValueHello_expectValueHELLO() {
+        //GIVEN
+        String a = "Hello";
+
+        //WHEN
+        String actual = Calculator.toUpperCase(a);
+
+        //THEN
+        assertEquals("HELLO",actual);
+
+    }
+    @Test
+    @DisplayName("isPositiveTest")
+    public void isPositiveTest_whenValue5_expectValueTrue() {
+        //GIVEN
+        int a = 5;
+
+        //WHEN
+        boolean actual = Calculator.isPositive(a);
+
+        //THEN
+        assertEquals(true,actual);
+
     }
 }
